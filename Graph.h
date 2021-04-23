06eee1829d893bd5150f1c131830a2e57fcb61d5
P-Balaramakrishna-Varma <balaramakrishna.p@students.iiit.ac.in>
2021-04-23 20:43:09 +0530

    Grahp and Minheap header files

---
+++ Graph.h
@@ -0,0 +1,52 @@
+#ifndef GRAPH
+#define GRAPH
+
+#include <stdbool.h>
+
+#define IntialSize_Graph 100
+
+typedef int *PtrBST;
+typedef int *PtrUser;
+typedef int *Minheap;
+
+
+
+
+typedef struct UserNode UserNode;
+typedef UserNode *PtrUserNode;
+
+struct UserNode
+{
+    int ID;
+    PtrBST OutVertices;                     // all friends of this user.
+    PtrBST InVertices;                      // all members whose friend is this user.
+    PtrUser User;                           // info about the user.
+};
+
+typedef struct GraphS GraphS;
+typedef struct GraphS *Graph;
+
+struct GraphS
+{
+    PtrUserNode *UserArray;                // pointer to Adjacency list
+    int MAX_Size;                          // book keeping for when to resize.
+    int LastId;                            // book keeping for when to resize.
+};
+
+
+
+
+
+
+
+Graph Init_Graph(Graph G);
+void Print_Graph(Graph G);
+void Delete_Graph(Graph *PtrG);
+
+void AddNode(int New_id, Graph G);
+void RemoveNode(int Del_Id, Graph G);
+void AddEdge(int A, int B, Graph G);
+bool checkEdge(int A, int B, Graph G);
+void PrintNode(int Node_id, Graph G);
+
+#endif
