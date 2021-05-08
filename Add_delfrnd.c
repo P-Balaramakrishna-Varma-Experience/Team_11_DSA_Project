08cf08998d53aaf6782485dd9f52e7da88413feb
Durga <82359931+Durga0024@users.noreply.github.com>
2021-05-08 18:21:29 +0530

    Update Add_delfrnd.c

--- Add_delfrnd.c
+++ Add_delfrnd.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include"Graph.h"
 #include"HashTable.h"
 
 void AddEdge(int A, int B, Graph G)
 {
 
-    void*FOUND=FindID(G->UserArray[A]->OutVertices,B);
+    int IsPresent=FindID(G->UserArray[A]->OutVertices,B);
 
-    if (FOUND==NULL)
+    if (FOUND==0)
     {
         AddElement(G->UserArray[A]->OutVertices, B);
         AddElement(G->UserArray[B]->InVertices, A);
@@ -19,9 +18,9 @@ void AddEdge(int A, int B, Graph G)
 
 void DeleteEdge(int A, int B, Graph G)
 {
-    void*FOUND=FindID(G->UserArray[A]->OutVertices,B);
+    int FOUND=IsPresent(G->UserArray[A]->OutVertices,B);
 
-    if (FOUND!=NULL)
+    if (FOUND!=0)
     {
         RemoveElement(G->UserArray[A]->OutVertices, B);
         RemoveElement(G->UserArray[B]->InVertices, A);
